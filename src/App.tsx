b92e2f9f3b1a78256f38cf86f1f4bdafa590da72
Gguife <gguife747@gmail.com>
2024-12-11 01:38:58 -0300

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import './global/style.css';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      Testando
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import './style.css';
+
+
+const Header = () => {
+
+  return (
+    <header>
+      <h2>PROFESSOR OLIVEIRA</h2>
+
+      <nav>
+        <ul>
+          <span>cursos</span>
+          <li><a href="">Mecânico de aeronaves</a></li>
+          <li><a href="">Piloto privado avião</a></li>
+          <li><a href="">Piloto privado Helicoptero</a></li>
+          <li><a href="">Comissário de voo</a></li>
+        </ul>
+        <a href="">Sobre mim</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
