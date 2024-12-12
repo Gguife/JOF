ac9c18e1a0f49e9380bece5f36700204d75940ad
Gguife <gguife747@gmail.com>
2024-12-12 03:17:44 -0300

    feat: Finished header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,36 @@
 import './style.css';
-
+import { useState } from 'react';
+import { IoIosArrowDown } from "react-icons/io";
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsVisible(false);  
+  }
 
   return (
     <header>
-      <h2>PROFESSOR OLIVEIRA</h2>
+      <h1>PROFESSOR OLIVEIRA</h1>
 
       <nav>
-        <ul>
-          <span>cursos</span>
-          <li><a href="">Mecânico de aeronaves</a></li>
-          <li><a href="">Piloto privado avião</a></li>
-          <li><a href="">Piloto privado Helicoptero</a></li>
-          <li><a href="">Comissário de voo</a></li>
-        </ul>
-        <a href="">Sobre mim</a>
+        <a href="" className='about'>Sobre mim</a>
+        <div className="cursos-container">
+          <p onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>cursos <IoIosArrowDown className='cursos-icon' /></p>
+          <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`container ${isVisible ? 'visible' : 'hidden'}`}>
+            <a href="" >Mecânico de aeronaves</a>
+            <hr />
+            <a href="">Piloto privado avião</a>
+            <hr />
+            <a href="">Piloto privado Helicóptero</a>
+            <hr />
+            <a href="">Comissário de voo</a>
+          </div>
+        </div>
       </nav>
     </header>
   )
