60e8f68b66b949af65f424baf77c58098709294d
Gguife <gguife747@gmail.com>
2024-12-12 22:44:09 -0300

    feat: Add new Pages in mainRouter and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import Footer from "./components/footer";
 import Header from "./components/header";
 import './global/style.css';
+import { MainRouter } from "./router/MainRouter";
 
 function App() {
 
   return (
     <>
       <Header />
-      Testando
+      <MainRouter />
+      <Footer />
     </>
   )
 }
