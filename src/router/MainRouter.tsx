500f700f7e400779850d91590cc4e0664075578c
Gguife <gguife747@gmail.com>
2024-12-12 22:50:20 -0300

    feat: Added Curso page

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,9 +1,11 @@
 import { useRoutes } from "react-router-dom"
 import Home from "../view/home";
+import CursoPage from "../view/cursosPDF";
 
 export const MainRouter = () => {
   const router = useRoutes([
-    {path: "/", element: <Home />}
+    {path: "/", element: <Home />},
+    {path: "/curso", element: <CursoPage />}
   ])
 
   return router;
